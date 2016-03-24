ce48e2c97d0ca2dd3430ad978f4f0f4b7b19c7ad
hushulin <hushulin12321@126.com>
2016-03-24 13:07:54 +0800

    init

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->get('/v' , function(){
+    $user = User::all();
+    var_dump($user);
+});
