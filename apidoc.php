d27558ed0dbb4f46f0d92ec532fbe2dd3249fc37
unknown <Administrator@.(none)>
2016-03-25 22:56:10 +0800

    0

--- apidoc.php
+++ apidoc.php
@@ -7,13 +7,14 @@ use Crada\Apidoc\Exception;
 $classes = array(
    'App\Http\Controllers\ExampleController',
    'App\Http\Controllers\UserController',
+   'App\Http\Controllers\MeterController',
 );
 
 $output_dir  = __DIR__.'/public/apidocs';
 $output_file = 'api.html'; // defaults to index.html
 
 try {
-    $builder = new Builder($classes, $output_dir, 'Api Title', $output_file);
+    $builder = new Builder($classes, $output_dir, '开德API(V1.0.0)', $output_file);
     $builder->generate();
 } catch (Exception $e) {
     echo 'There was an error generating the documentation: ', $e->getMessage();
